e4c77539a843070d31464bcc3326a40bc32614a5
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-21 10:51:56 -0400

    stub up function

--- chapter-1/arraysAndStrings.js
+++ chapter-1/arraysAndStrings.js
@@ -1,6 +1,10 @@
 // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+function isUnique() {
+  console.log('check')
+}
 
+console.log(isUnique())
 
 // 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the others. 
 
