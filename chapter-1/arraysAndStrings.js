26da86f3044149f6a46e489ca870e596a92c0710
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-21 10:57:22 -0400

    write out examples

--- chapter-1/arraysAndStrings.js
+++ chapter-1/arraysAndStrings.js
@@ -1,10 +1,13 @@
 // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
-function isUnique() {
+function isUnique(str) {
   console.log('check')
+
+  
 }
 
-console.log(isUnique())
+console.log(isUnique('hello')) // should return false
+console.log(isUnique('sup')) // should return false
 
 // 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the others. 
 
